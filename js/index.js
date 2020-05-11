e2573de85d28c43f7fbd75d339e4a780e98338d3
Maya Chisem <chisem.maya@gmail.com>
2020-05-11 15:24:59 -0700

    update

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var slideIndex = 0;
+showImg();
+
+function showImg() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showImg, 4000); 
+}
+
